b01c4099f47e91de908dd950d780b2632a0513e6
musharofahmed1 <musharofahmed11@gmail.com>
2023-08-18 07:30:16 -0600

    function declare

--- reverseNumber.cpp
+++ reverseNumber.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
+//create a function for reverse a number
+int reverseNumber(int n, int reversedNum = 0){
+ 
+ }
 int main(){
-    
+
+    //getting number from user
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+
     return 0;
 }
