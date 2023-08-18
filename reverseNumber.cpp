048fda4e23029ea161ec4a621ae14e5a3a535029
musharofahmed1 <musharofahmed11@gmail.com>
2023-08-18 07:51:28 -0600

    semi complete

--- reverseNumber.cpp
+++ reverseNumber.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 //create a function for reverse a number
-int reverseNumber(int n, int reversedNum = 0){
- 
+int reverseNumber(int n,int reversedNum = 0){
+   
+     if (n == 0) {
+        return reversedNum;
+    } else {
+        int lastDigit = n % 10;
+        reversedNum = reversedNum * 10 + lastDigit;
+        return reverseNumber(n / 10, reversedNum);
+    }
  }
 int main(){
 
@@ -10,6 +17,10 @@ int main(){
     int num;
     cout << "Enter a number: ";
     cin >> num;
+
+    //call the function
+     int reversedNum = reverseNumber(num);
+     cout << "Reversed number: " << reversedNum << endl;
 
 
     return 0;
